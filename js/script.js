ae66597dd0237cfc8dde368908ff507543a06a87
Silje <silje.eidsvoll@gmail.com>
2023-04-06 10:15:14 +0200

    Add grogeri in list

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const handeliste = document.querySelector("handleliste");
 // Hente Input objekt
 const inpVare = document.querySelector("inpVare");
 
+inpVare.ocechange = () => {
+    handeliste.innerHTML += `
+     <li> ${inpVare.value} <li/>
+
+    `;
+
+    inpVare.value = "";
+
+}
