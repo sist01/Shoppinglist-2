40aa174355d9e5377e7172c6f03ac83f3e5d71db
Silje <silje.eidsvoll@gmail.com>
2023-04-06 11:01:06 +0200

    add button function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 // Hente UL objekt
-const handeliste = document.querySelector("handleliste");
+const handleliste = document.querySelector("#handleliste");
 
 // Hente Input objekt
-const inpVare = document.querySelector("inpVare");
+const inpVare = document.querySelector("#inpVare");
 
-inpVare.ocechange = () => {
-    handeliste.innerHTML += `
-     <li> ${inpVare.value} <li/>
-
+inpVare.onchange = () => {
+    handleliste.innerHTML += `
+        <li> ${inpVare.value} </li>
     `;
-
+    
     inpVare.value = "";
+};
 
-}
+function addButton() {
+    const liElement = document.createElement('li');
+    if(inputElement.value !=='') {
+        liElement.textContent = inputElement.value;
+        ulShoppingListElement.appendChild(liElement);
+    }
+} 
