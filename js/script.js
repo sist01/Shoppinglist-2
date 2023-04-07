257e0636cc3c3276a2b48874ea65b5c0769a6600
Silje <silje.eidsvoll@gmail.com>
2023-04-07 14:51:34 +0200

    checkbox, localstorage, css, clear button

--- js/script.js
+++ js/script.js
@@ -3,19 +3,48 @@ const handleliste = document.querySelector("#handleliste");
 
 // Hente Input objekt
 const inpVare = document.querySelector("#inpVare");
+const clearKnapp = document.querySelector("#clearKnapp");
+
+handleliste.innerHTML = localStorage.getItem("handleliste");
+
+var id = localStorage.getItem("teller");
+if (id === null) {
+    id = 1;
+}
 
 inpVare.onchange = () => {
-    handleliste.innerHTML += `
-        <li> ${inpVare.value} </li>
-    `;
-    
-    inpVare.value = "";
+    if (inpVare.value !== '') {
+        
+        var vare = ` <li id="button`+ id + `"><button class="checkbox" onClick="check(`+ id + `)">X</button> ${inpVare.value} </li>`;
+        handleliste.innerHTML += vare;
+        var nyeVarer = "";
+        var gamleVarer = localStorage.getItem("handleliste");
+        console.log("Gamle varer: " + gamleVarer);
+        if (gamleVarer !== null) {
+            nyeVarer = gamleVarer;
+        }
+        nyeVarer = nyeVarer + vare;
+        localStorage.setItem("handleliste", nyeVarer)
+        localStorage.setItem("teller", id++);
+        inpVare.value = "";
+    }
 };
 
-function addButton() {
-    const liElement = document.createElement('li');
-    if(inputElement.value !=='') {
-        liElement.textContent = inputElement.value;
-        ulShoppingListElement.appendChild(liElement);
+clearKnapp.onclick = () => {
+    handleliste.innerHTML = "";
+    console.log("tømmer");
+    localStorage.clear();
+}
+
+function check(id) {
+    const listElement = document.querySelector('#button' + id);
+    if (listElement.hasAttribute("style")) {
+        listElement.removeAttribute("style");
+    } else {
+        listElement.setAttribute("style", 'text-decoration:line-through');
     }
-} 
+    localStorage.setItem("handleliste", handleliste.innerHTML);
+}
+
+
+console.log("hei på deg ");
